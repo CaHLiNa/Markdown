fe87ea3fded461ca296884dad7f83b577c3e7fda
Feng Baobao <2114039928@qq.com>
2026-03-07 19:37:43 +0800

    Initial Commit

---
+++ Markdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Markdown
+//
+//  Created by Math73SR on 2026/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Markdown/MarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownApp.swift
+//  Markdown
+//
+//  Created by Math73SR on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
